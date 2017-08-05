c978f8586e4690a42af274c85912e3499a1a7c60
Andres Root <info@andresroot.co>
2017-08-05 16:00:43 -0500

    Working on dataset loader

--- tensor.py
+++ tensor.py
@@ -2,12 +2,12 @@ from keras.preprocessing import image
 from tqdm import tqdm
 
 def path_to_tensor(img_path):
-	img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(224, 224))
 
-	x = image.img_to_array(img)
+    x = image.img_to_array(img)
 
-	return np.expand_dims(x, axis=0)
+    return np.expand_dims(x, axis=0)
 
 def paths_to_tensor(img_paths):
-	list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
