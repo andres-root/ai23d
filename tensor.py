5ecb7b68c0e6aa3f96269f60935d6b10e33dd64b
luis.bejarano <luis.bejarano@globant.com>
2017-08-05 15:44:10 -0500

    path_to_tensor, paths_to_tensor

---
+++ tensor.py
@@ -0,0 +1,13 @@
+from keras.preprocessing import image
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+	img = image.load_img(img_path, target_size=(224, 224))
+
+	x = image.img_to_array(img)
+
+	return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+	list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
