6c93f301ae907ca0c5e79886ecdda6322ea11b12
luis.bejarano <luis.bejarano@globant.com>
2017-08-06 01:25:39 -0500

    retrofit, image upload

---
+++ images/bones/app/ai23d.ipynb/app/src/main/java/bones/aito3d/Constants.java
@@ -0,0 +1,11 @@
+package bones.aito3d;
+
+/**
+ * Created by luisalfonsobejaranosanchez on 8/6/17.
+ */
+
+public class Constants {
+
+    public static String AI_ENDPOINT = "http://192.168.0.234:3000";
+
+}
--- images/bones/app/ai23d.ipynb/app/src/main/java/bones/aito3d/Service.java
+++ images/bones/app/ai23d.ipynb/app/src/main/java/bones/aito3d/Service.java
@@ -4,5 +4,19 @@ package bones.aito3d;
  * Created by luisalfonsobejaranosanchez on 8/5/17.
  */
 
-public class Service {
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+interface  Service {
+
+    @Multipart
+    @POST("/")
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("name") RequestBody name);
+
 }
