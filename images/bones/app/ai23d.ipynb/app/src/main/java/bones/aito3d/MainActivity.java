184d5b48c467a5266533581bc3192e6f05454b30
luis.bejarano <luis.bejarano@globant.com>
2017-08-05 22:33:40 -0500

    app

---
+++ images/bones/app/ai23d.ipynb/app/src/main/java/bones/aito3d/MainActivity.java
@@ -0,0 +1,13 @@
+package bones.aito3d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
